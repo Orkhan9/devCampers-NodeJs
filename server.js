efcbc7e0b554ec9837366d402847e9eed8cd97cd
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-01-25 02:49:44 +0400

    added bootcamp model CRUD

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan=require('morgan');
-const colors=require('colors');
-const connectDB=require('./config/db')
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDB();
 
-//Route files
-const bootcamps = require("./routes/bootcamps");
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-//Mount routers
-app.use("/api/v1/bootcamps", bootcamps);
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-const server=app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running on port ${PORT} ${process.env.NODE_ENV} `.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
-// Handle unhandle promise rejections
-process.on('unhandledRejection',(err,promise)=>{
-  console.log(`Error:${err.message}`.red);
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
-  server.close(()=>process.exit(1));
-})
+  // server.close(() => process.exit(1));
+});
