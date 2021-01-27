2b1506be6ed047380ef28f0d447a6565331dc6e5
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-01-27 23:56:53 +0400

    added Course routes & getCourses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const {
   getBootcamps,
   getBootcamp,
@@ -6,18 +6,27 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius
-} = require("../controllers/bootcamps");
+} = require('../controllers/bootcamps');
+
+// Include other resource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+    .route('/')
+    .get(getBootcamps)
+    .post(createBootcamp);
 
 router
-  .route("/:id")
-  .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 module.exports = router;
